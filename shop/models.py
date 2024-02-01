dac94dc94ef1db3f854f3839ac2cb5032754a3e0
DzygunRoman <dzygun-roman@mail.ru>
2024-02-01 09:49:52 +0300

    создание модели категорий и  модели товара

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Категория')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Слаг')
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, verbose_name='Название продукта')
+    slug = models.SlugField(max_length=200, verbose_name='Слаг')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')#Для хранения денежных едениц всегда использовать decimal
+    available = models.BooleanField(default=True, verbose_name='Доступность')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
