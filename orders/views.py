e6d3017b777c4ea14af461227f9d51ac7b3ee823
DzygunRoman <dzygun-roman@mail.ru>
2024-02-06 09:36:55 +0300

    метод order_create в views.py приложения Orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)# Текущая корзина извлекается из сеанса
+    if request.method == 'POST':# В базе создается заказ
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            #очистить корзину
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()# содается экземпляр формы и прорисовывается шаблон
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+
