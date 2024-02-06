62aac541c17e3b4e6e16a8596065dfcd9abecc75
DzygunRoman <dzygun-roman@mail.ru>
2024-02-06 14:58:40 +0300

    подключаю celery

--- myshop/celery.py
+++ myshop/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 
 #Задать стандартный модуль настроек Django для программы 'celery'
-os.environ.setdefault('DJANG0_SETTINGS_MODULE', 'myshop.settings')# для встроенной в celery командной строки
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')# для встроенной в celery командной строки
 app = Celery('myshop')# Создается экземпляр приложения
 app.config_from_object('django.conf:settings', namespace='CELERY')# Загружается любая конфигурация из проекта и префикс для Celery
-app.autodiscover_tasks()# Ищет файл tasks.py в каждом приложении и автоматически обнаруживает асинхронные задания
+app.autodiscover_tasks()# Ищет Celery Wordjango.core.exceptions.ImproperlyConfigured: Requested settings, but settings are not configured. You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.kerфайл tasks.py в каждом приложении и автоматически обнаруживает асинхронные задания
