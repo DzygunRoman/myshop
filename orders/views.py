1a0c6ccc26315cff512d979222bd24de3c5bbbce
DzygunRoman <dzygun-roman@mail.ru>
2024-02-07 11:19:17 +0300

    добавил в views.py приложения orders метод admin_order_detail для отображения информации о заказе в админ-панели

--- orders/views.py
+++ orders/views.py
@@ -1,26 +1,32 @@
-from django.shortcuts import render
-from .models import OrderItem
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def order_create(request):
-    cart = Cart(request)# Текущая корзина извлекается из сеанса
-    if request.method == 'POST':# В базе создается заказ
+    cart = Cart(request)  # Текущая корзина извлекается из сеанса
+    if request.method == 'POST':  # В базе создается заказ
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-            #очистить корзину
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+                                         quantity=item['quantity'])
+            # очистить корзину
             cart.clear()
             # Запустить асинхронное задание
             order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
-        form = OrderCreateForm()# содается экземпляр формы и прорисовывается шаблон
+        form = OrderCreateForm()  # содается экземпляр формы и прорисовывается шаблон
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
 
 
-
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
