d0dc92c8390ecfa97b054219d6ccbda55d7d0610
DzygunRoman <dzygun-roman@mail.ru>
2024-02-08 13:36:07 +0300

    добавил купон в представление детальной информации заказа

--- cart/views.py
+++ cart/views.py
@@ -32,4 +32,6 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(initial={
                             'quantity': item['quantity'],
                             'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart,
+                                                'coupon_apply_form': coupon_apply_form})
