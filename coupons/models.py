39058669d9be31817e4094e42eb82048758bb133
DzygunRoman <dzygun-roman@mail.ru>
2024-02-08 09:16:50 +0300

    создал модель купона

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True, verbose_name='Код')
+    valid_from = models.DateTimeField(verbose_name='Действителен с ')
+    valid_to = models.DateTimeField(verbose_name='Действителен до ')
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)],help_text='Значение в процентах (0 до 100)')
+    active = models.BooleanField(verbose_name='Cтатус')
+
+    def __str__(self):
+        return self.code
+
