e39e2399481fc9022f089760dd722a6b13282086
DzygunRoman <dzygun-roman@mail.ru>
2024-02-08 11:09:53 +0300

    импортировал в представление форму купона

--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
-
+from coupons.forms import CouponApplyForm
 
 @require_POST
 def cart_add(request, product_id):
